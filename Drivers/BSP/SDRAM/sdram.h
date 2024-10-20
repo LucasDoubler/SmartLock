a11ee833a5f5c9a3f5baca3ebece156f776ff625
Lucas_Doubler <gym_lucas@163.com>
2024-10-20 19:19:14 +0800

    Update sdram.h

--- Drivers/BSP/SDRAM/sdram.h
+++ Drivers/BSP/SDRAM/sdram.h
@@ -1,26 +1,4 @@
-/**
- ****************************************************************************************************
- * @file        sdram.h
- * @author      正点原子团队(ALIENTEK)
- * @version     V1.0
- * @date        2022-09-06
- * @brief       SDRAM 驱动代码
- * @license     Copyright (c) 2020-2032, 广州市星翼电子科技有限公司
- ****************************************************************************************************
- * @attention
- *
- * 实验平台:正点原子 阿波罗 H743开发板
- * 在线视频:www.yuanzige.com
- * 技术论坛:www.openedv.com
- * 公司网址:www.alientek.com
- * 购买地址:openedv.taobao.com
- *
- * 修改说明
- * V1.0 20220906
- * 第一次发布
- *
- ****************************************************************************************************
- */
+
 
 #ifndef _SDRAM_H
 #define _SDRAM_H
@@ -28,11 +6,11 @@
 #include "./SYSTEM/sys/sys.h"
 
 
-extern SDRAM_HandleTypeDef g_sram_handle;               /* SDRAM句柄 */
+extern SDRAM_HandleTypeDef g_sram_handle;               /* SDRAM鍙ユ焺 */
 
-#define BANK5_SDRAM_ADDR    ((uint32_t)(0XC0000000))    /* SDRAM开始地址 */
+#define BANK5_SDRAM_ADDR    ((uint32_t)(0XC0000000))    /* SDRAM寮�濮嬪湴鍧� */
 
-/* SDRAM配置参数 */
+/* SDRAM閰嶇疆鍙傛暟 */
 #define SDRAM_MODEREG_BURST_LENGTH_1             ((uint16_t)0x0000)
 #define SDRAM_MODEREG_BURST_LENGTH_2             ((uint16_t)0x0001)
 #define SDRAM_MODEREG_BURST_LENGTH_4             ((uint16_t)0x0002)
